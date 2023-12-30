00ec0b3855b62012fb604ba10177079ba9e4a21a
builtbysuraj <surajgupta3553@gmail.com>
2023-12-30 19:07:25 +0530

    feat: setup folder structures and connect to MongoDB
    Connect to MongoDB Atlas, configure Prettier and Eslint

--- server/config.js
+++
@@ -1,4 +0,0 @@
-import dotenv from "dotenv"
-dotenv.config()
-
-export const POSGRESQL_DB_CONNECT_URL = process.env.POSGRESQL_DB_CONNECT_URL
---
+++ server/src/index.js
@@ -0,0 +1,18 @@
+import dotenv from 'dotenv'
+dotenv.config({
+  path: './.env',
+})
+
+import app from './app.js'
+import connectDB from './db/db.js'
+
+const PORT = process.env.PORT || 5000
+
+connectDB()
+  .then(() => {
+    app.listen(PORT, () =>
+      console.log(`\n ⚙️ Server is running at port : ${PORT}🎉`),
+    )
+  })
+  .catch((err) => console.log('MONGO db connection failed ! ', err))
+
