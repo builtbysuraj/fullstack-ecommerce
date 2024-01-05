b4fb7ff70a7e73d74b0d97de1e657371ae38d633
builtbysuraj <surajgupta3553@gmail.com>
2024-01-05 18:47:06 +0530

    style: Add cart, flipkart icon in header

--- client/src/layouts/Header.tsx
+++ client/src/layouts/Header.tsx
@@ -1,15 +1,17 @@
-import { AppBar, Container, TextField } from '@mui/material'
+import { AppBar, Box, Button, Container, TextField } from '@mui/material'
 import { Link } from 'react-router-dom'
 
+import cart from '../assets/img/cart.svg'
 import useHandleDispatch from '../hooks/useHandleDispatch'
 import { useAppSelector } from '../state/store'
+import flipkart from '/flipkart.png'
 
 export default function Header() {
   const stateData = useAppSelector((state) => state.filtersReducer)
   const { handleSearchQuery } = useHandleDispatch()
 
   return (
-    <AppBar elevation={1} position="sticky" sx={{ height: '4rem' }}>
+    <AppBar elevation={1} position="sticky" sx={{}}>
       <Container>
         <div
           style={{
@@ -18,20 +20,44 @@ export default function Header() {
             justifyContent: 'space-between',
           }}
         >
-          <h3>
-            <Link to="/">Flipkart</Link>
-          </h3>
+          <Box
+            display="flex"
+            // justifyContent="center"
+            flexDirection="column"
+            alignItems="center"
+          >
+            <Link to="/">
+              <img width="75" src={flipkart} alt="Flipkart" title="Flipkart" />
+            </Link>
+          </Box>
           <TextField
-            sx={{ bgcolor: 'white', m: 1, width: '100%' }}
-            placeholder="Search product or category"
+            sx={{ bgcolor: 'white', m: 1 }}
+            placeholder="Search for products, brands and more"
             size="small"
-            fullWidth
             value={stateData.searchQuery}
             onChange={(e) => handleSearchQuery(e.target.value)}
           />
+          <Button
+            variant="text"
+            sx={{
+              bgcolor: 'white',
+              color: 'black',
+            }}
+          >
+            Login
+          </Button>
+          <Button variant="text" sx={{ color: 'white' }}>
+            Become a Seller
+          </Button>
+          <Button variant="text" sx={{ color: 'white' }}>
+            More
+          </Button>
           <div>
             <Link to="/products"> Product Listing </Link>
-            <Link to="/cart"> Cart </Link>
+            <Link to="/cart">
+              <img src={cart} alt="Cart" />
+              Cart
+            </Link>
           </div>
         </div>
       </Container>
