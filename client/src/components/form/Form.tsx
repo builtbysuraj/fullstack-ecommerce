8c07eea75ecd59255e39dc8035df60e430b092c7
builtbysuraj <surajgupta3553@gmail.com>
2024-02-12 20:25:42 +0530

    feat: Integrate login and signup api
    Remove proxy from vite.config.ts and add client url to server cors origin

--- client/src/components/form/Form.tsx
+++ client/src/components/form/Form.tsx
@@ -1,5 +1,10 @@
+import axios from 'axios'
 import { Link } from 'react-router-dom'
+
+import { ENV } from '@/conf'
 import styles from './Form.module.css'
+
+axios.defaults.withCredentials = true
 
 type FormProps = {
   mode: string
@@ -14,6 +19,21 @@ export default function From({
   promptLink,
   promptLinkText,
 }: FormProps) {
+  const endpoint = mode === 'Signup' ? '/register' : '/login'
+
+  const handleSubmit = async (evt: React.FormEvent<HTMLFormElement>) => {
+    evt.preventDefault()
+    const form = evt.target as HTMLFormElement
+    const formData = new FormData(form)
+
+    // Extracting form values through 'name' property, in 'data'
+    const data = Object.fromEntries(formData)
+    const res = await axios.post(`${ENV.SERVER_URL}/${endpoint}`, data)
+    console.log(res.data)
+
+    form.reset()
+  }
+
   return (
     <div className={styles.formContainer}>
       <div className={styles.left}>
@@ -26,16 +46,21 @@ export default function From({
         </div>
       </div>
       <div className={styles.right}>
-        <form className={styles.form}>
-          <input type="text" placeholder="Username" />
-          <input type="password" placeholder="Password" />
+        <form onSubmit={handleSubmit} className={styles.form}>
+          <input type="text" placeholder="Username" name="username" required />
+          <input
+            type="password"
+            placeholder="Password"
+            name="password"
+            required
+          />
           <small className={styles.fromSmall}>
             By continuing, you agree to Flipkart's
             <span className={styles.demoLink}> Terms of Use </span>
             and
             <span className={styles.demoLink}> Privacy Policy.</span>
           </small>
-          <button>{mode}</button>
+          <button type="submit">{mode}</button>
           <p>
             {promptText}{' '}
             <Link to={promptLink} className={styles.demoLink}>
