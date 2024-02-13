3fbf9bb44f35658e9ca12298a3124bd251aa72af
builtbysuraj <surajgupta3553@gmail.com>
2024-02-13 23:43:25 +0530

    feat: Enable only logged-in users to place orders

--- server/src/model/payment.model.ts
+++ server/src/model/payment.model.ts
@@ -14,6 +14,11 @@ const paymentSchema = new mongoose.Schema(
       type: String,
       required: true,
     },
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User',
+      required: true,
+    },
   },
   { timestamps: true }
 )
